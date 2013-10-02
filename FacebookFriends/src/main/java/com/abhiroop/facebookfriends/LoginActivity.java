b0a6a55031a81d6fbc835350bf3c0f5fe4dcceb9
abhirooppatel <abhiroop.patel@gmail.com>
2013-10-02 13:03:05 +0530

    Renamed MainActivity to LoginActivity

--- FacebookFriends/src/main/java/com/abhiroop/facebookfriends/LoginActivity.java
+++ FacebookFriends/src/main/java/com/abhiroop/facebookfriends/LoginActivity.java
@@ -1,22 +1,16 @@
 package com.abhiroop.facebookfriends;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
-import com.facebook.LoggingBehavior;
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Settings;
-import com.facebook.model.GraphUser;
 
-public class MainActivity extends SherlockActivity {
+public class LoginActivity extends SherlockActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 
     @Override
